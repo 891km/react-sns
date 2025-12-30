7a74222774f90b4a6d444989ee64d29777706876
김민주 <891km@ewhain.net>
2025-12-31 04:14:06 +0900

    feat: 세션 관리를 위한 session provider 추가

--- src/components/ui/app-loader.tsx
+++ src/components/ui/app-loader.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/ui/spinner";
 
 export default function AppLoader() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="fixed top-0 left-0 z-9999 flex h-screen w-screen items-center justify-center bg-white/40">
       <Spinner className="h-8 w-8" />
     </div>
   );
