d1fd500f231483f14a86009541ff3d3b5d080a41
김민주 <891km@ewhain.net>
2026-01-04 23:10:44 +0900

    chore: 파비콘 및 타이틀 변경

--- src/pages/not-found-page.tsx
+++ src/pages/not-found-page.tsx
@@ -1,3 +1,14 @@
+import { TriangleAlert } from "lucide-react";
+
 export default function NotFoundPage() {
-  return <>NotFound</>;
+  return (
+    <div className="fixed top-0 left-0 z-9999 flex h-screen w-screen items-center justify-center bg-white/40">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <TriangleAlert className="h-7 w-7 text-red-400" />
+        <div className="text-center whitespace-pre-line">
+          페이지를 찾을 수 없습니다.
+        </div>
+      </div>
+    </div>
+  );
 }
