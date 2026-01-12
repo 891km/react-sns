ce74d9ecce9cfc716fb966b0bcac106be1c780a8
김민주 <891km@ewhain.net>
2026-01-12 14:08:59 +0900

    fix: hidden content 렌더링 hook 오류 수정

--- src/components/post-item/post-text-contents.tsx
+++ src/components/post-item/post-text-contents.tsx
@@ -1,7 +1,8 @@
 import { POST_CONTENT_LENGTH_SHORT } from "@/constants/constants";
-import { renderHiddenContent } from "@/lib/hide-content";
 import type { PostEntity, PostType } from "@/types/types";
-import { useState } from "react";
+import { cn } from "@/lib/utils";
+import type { ContentMeta } from "@/types/types";
+import { useState, type MouseEvent, type ReactNode } from "react";
 
 export default function PostTextContents({
   post,
@@ -38,14 +39,73 @@ export default function PostTextContents({
             </>
           ) : (
             <>
-              {renderHiddenContent({
-                content: post.content,
-                contentMeta: post.metadata?.content_hidden,
-              })}
+              {
+                <HiddenContent
+                  content={post.content}
+                  contentMeta={post.metadata?.content_hidden}
+                />
+              }
             </>
           )}
         </p>
       )}
     </>
   );
+}
+
+function HiddenContent({
+  content,
+  contentMeta,
+}: {
+  content: string;
+  contentMeta?: ContentMeta;
+}) {
+  const [isShown, setIsShown] = useState<boolean[]>(() =>
+    Array(contentMeta?.length ?? 0).fill(false),
+  );
+
+  const handleShowClick = (e: MouseEvent<HTMLSpanElement>, index: number) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setIsShown((prev) =>
+      prev.map((isShown, i) => (i === index ? !isShown : isShown)),
+    );
+  };
+
+  if (contentMeta?.length === 0) return content;
+
+  let nodes: ReactNode[] = [];
+  let cursor = 0;
+
+  contentMeta?.forEach((meta, index) => {
+    if (cursor < meta.start) {
+      nodes.push(
+        <span key={`text-${index}`}>{content.slice(cursor, meta.start)}</span>,
+      );
+    }
+
+    nodes.push(
+      <span
+        key={`hidden-${index}`}
+        className={cn(
+          isShown[index]
+            ? "text-primary bg-muted-foreground/10"
+            : "bg-muted-foreground/40 rounded-xs text-transparent",
+          "cursor-pointer",
+          "transition-colors duration-150",
+        )}
+        onClick={(e) => handleShowClick(e, index)}
+      >
+        {content.slice(meta.start, meta.end)}
+      </span>,
+    );
+    cursor = meta.end;
+  });
+
+  if (cursor < content.length) {
+    nodes.push(<span key="text-end">{content.slice(cursor)}</span>);
+  }
+
+  return nodes;
 }
--- src/lib/hide-content.tsx
+++ src/lib/hide-content.tsx
@@ -4,9 +4,8 @@
  * after → noop
  */
 
-import { cn } from "@/lib/utils";
 import type { ContentMeta } from "@/types/types";
-import { useState, type MouseEvent, type ReactNode } from "react";
+import type { ReactNode } from "react";
 
 export function adjustContentMeta({
   delta,
@@ -104,63 +103,6 @@ export function renderTextareaOverlay({
       <span
         key={`hidden-${index}`}
         className="bg-muted-foreground/30 rounded-xs"
-      >
-        {content.slice(meta.start, meta.end)}
-      </span>,
-    );
-    cursor = meta.end;
-  });
-
-  if (cursor < content.length) {
-    nodes.push(<span key="text-end">{content.slice(cursor)}</span>);
-  }
-
-  return nodes;
-}
-
-export function renderHiddenContent({
-  content,
-  contentMeta,
-}: {
-  content: string;
-  contentMeta?: ContentMeta;
-}) {
-  const [isShown, setIsShown] = useState<boolean[]>(() =>
-    Array(contentMeta?.length ?? 0).fill(false),
-  );
-
-  const handleShowClick = (e: MouseEvent<HTMLSpanElement>, index: number) => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    setIsShown((prev) =>
-      prev.map((isShown, i) => (i === index ? !isShown : isShown)),
-    );
-  };
-
-  if (contentMeta?.length === 0) return content;
-
-  let nodes: ReactNode[] = [];
-  let cursor = 0;
-
-  contentMeta?.forEach((meta, index) => {
-    if (cursor < meta.start) {
-      nodes.push(
-        <span key={`text-${index}`}>{content.slice(cursor, meta.start)}</span>,
-      );
-    }
-
-    nodes.push(
-      <span
-        key={`hidden-${index}`}
-        className={cn(
-          isShown[index]
-            ? "text-primary bg-muted-foreground/10"
-            : "bg-muted-foreground/40 rounded-xs text-transparent",
-          "cursor-pointer",
-          "transition-colors duration-150",
-        )}
-        onClick={(e) => handleShowClick(e, index)}
       >
         {content.slice(meta.start, meta.end)}
       </span>,
